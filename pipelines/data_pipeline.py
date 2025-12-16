9f98900544fd37b7eb6178d0497c993d3b6bce26
Mahendra-Jangid-ai <mahendrakumar24325@gmail.com>
2025-12-16 17:07:07 +0530

    COMMIT

--- pipelines/data_pipeline.py
+++ pipelines/data_pipeline.py
@@ -20,6 +20,7 @@ def run_pipeline() -> None:
     df["Experience"] = df["Experience"].str.replace("years", "", regex=False)
     df['Experience'] = pd.to_numeric(df['Experience'])
     df["Experience"] = df["Experience"].fillna(0).astype("int64")
+    
     # bins = [5,10,15,20,25,30,35,40]
     # labels = ['Experience 1-5 year','Experience 5-10 year','Experience 10-15 year','Experience 15-20 year','Experience 20-25 year','Experience 25-30 year','Experience 30-35 year','Experience 35-40 year']
     # df['Experience'] = pd.cut(df['Experience'], bins=bins, labels=labels, right=True)
